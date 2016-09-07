7d42a8aba3edf510b29035df6a6dd68323297d95
Koki Takahashi <hakatasiloving@gmail.com>
2016-09-07 22:14:51 +0900

    Implement data loading from csv

--- build-py3.py
+++ build-py3.py
@@ -1,17 +1,24 @@
 import fontforge
 import psMat
 import csv
+import os
 
 with open('ligatures.csv', 'rb') as file:
-    ligatures = csv.reader(file)
+    ligatures = csv.reader(file, delimiter=' ')
+    glyphs = [glyph for [ligature, glyph] in ligatures]
 
 ricty = fontforge.open('RictyDiminished/RictyDiminished-Regular.ttf')
 fira = fontforge.open('FiraCode/distr/otf/FiraCode-Regular.otf')
 
-ricty.createChar(-1, 'equal_greater.liga')
-fira.selection.select('equal_greater.liga')
-fira.copy()
-ricty.selection.select('equal_greater.liga')
-ricty.paste()
+for glyph in glyphs:
+    ricty.createChar(-1, glyph)
+    fira.selection.select(glyph)
+    fira.copy()
+    ricty.selection.select(glyph)
+    ricty.paste()
 
+try:
+    os.remove('test.ttf')
+except OSError:
+    pass
 ricty.generate('test.ttf')
