150edcfc188eac6ca9d18909eafca276ccea0166
Koki Takahashi <hakatasiloving@gmail.com>
2016-09-07 22:41:09 +0900

    Write compiled ligature data to ligatures.json

--- build-py3.py
+++ build-py3.py
@@ -1,14 +1,32 @@
+# -*- coding: utf-8 -*-
+
 import fontforge
 import psMat
+import json
 import csv
 import os
 
-with open('ligatures.csv', 'rb') as file:
-    ligatures = csv.reader(file, delimiter=' ')
-    glyphs = [glyph for [ligature, glyph] in ligatures]
-
 ricty = fontforge.open('RictyDiminished/RictyDiminished-Regular.ttf')
 fira = fontforge.open('FiraCode/distr/otf/FiraCode-Regular.otf')
+
+with open('ligatures.csv', 'rb') as file:
+    ligatures_reader = csv.reader(file, delimiter=' ')
+
+    ligatures = []
+    glyphs = []
+
+    for [components, glyph] in ligatures_reader:
+        glyphs.append(glyph)
+        component_names = list(map(lambda c: ricty[ord(c)].glyphname, list(components)))
+
+        ligatures.append({
+            'glyph': glyph,
+            'components': component_names,
+            'lookup': '_'.join(map(lambda name: name.upper(), component_names)),
+        })
+
+with open('ligatures.json', 'w') as file:
+    file.write(unicode(json.dumps(ligatures)))
 
 for glyph in glyphs:
     ricty.createChar(-1, glyph)
